0b536cc473fff96d594d933ab1375e5a9c2951d7
kdtTetz <xenosign@naver.com>
2023-12-01 00:05:54 +0900

    Update ch11.js

--- 4w/ch11.js
+++ 4w/ch11.js
@@ -1,41 +1,41 @@
-// function saveUserData(user) {
-//   console.log('USER', user);
-//   throw 'err';
-// }
+function saveUserData(user) {
+  console.log("USER", user);
+  throw "err";
+}
 
-// function logToSnapErr(err) {
-//   console.log('ERR', err);
-// }
+function logToSnapErr(err) {
+  console.log("ERR", err);
+}
 
-// function wrapLogging(func) {
-//   return function (arg) {
-//     try {
-//       console.log('arg', arg)
-//       return func(arg);
-//     } catch (err) {
-//       logToSnapErr(err);
-//     }
-//   }
-// }
+function wrapLogging(func) {
+  return function (arg) {
+    try {
+      console.log("arg", arg);
+      return func(arg);
+    } catch (err) {
+      logToSnapErr(err);
+    }
+  };
+}
 
-// const saveUserDataWithLog = wrapLogging(saveUserData);
-// saveUserDataWithLog('lhs');
+const saveUserDataWithLog = wrapLogging(saveUserData);
+saveUserDataWithLog("lhs");
 
-// let inc = makeAdder(1);
+let inc = makeAdder(1);
 
-// function makeAdder(n) {
-//   return function (x) {
-//     return n + x;
-//   }
-// }
+function makeAdder(n) {
+  return function (x) {
+    return n + x;
+  };
+}
 
-// const result = inc(10);
+const result = inc(10);
 
-// console.log(result);
+console.log(result);
 
 function makeErr(a1, a2, a3) {
   console.log(...arguments);
-  throw 'err';
+  throw "err";
 }
 
 function wrapIgnoreErr(func) {
@@ -50,8 +50,22 @@ function wrapIgnoreErr(func) {
 
 const ignoreFunc = wrapIgnoreErr(makeErr);
 
-// function ignoreFunc(a1, a2, a3) {
-//   return wrapIgnoreErr(makeErr);
-// }
-
 ignoreFunc(1, 2, 3);
+
+// 연습
+function writeConsole(test, arg1, arg2, arg3) {
+  console.log(...arguments);
+}
+
+function wrapFunc(func) {
+  return function (test, a1, a2, a3) {
+    if (test) {
+      func(...arguments);
+    } else {
+      console.log("ELSE");
+    }
+  };
+}
+
+const wrapped = wrapFunc(writeConsole);
+wrapped(false, 1, 2, 3);
