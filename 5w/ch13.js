149930e227a651a2f78a4b08c5ce7f916d2b3a7e
kdtTetz <xenosign@naver.com>
2023-12-06 05:03:39 +0900

    ch13 정리

--- 5w/ch13.js
+++ 5w/ch13.js
@@ -1,15 +1,18 @@
-const { reduce } = require("lodash");
+const { reduce, forEach } = require("lodash");
 
 const arr = [1, 2, 3, 4];
 const objArr = [{ total: 1 }, { total: 2 }, { total: 3 }, { total: 4 }];
 
 // 수업 코드와 비슷하게 맞추기 위해 Lodash reduce 함수 사용, 인자 순서 다름
 function maxKey(arr, init, foo) {
-  return reduce(arr, function (max, el) {
-    if (foo(max) > foo(el))
-      return max;
-    return el;
-  }, init);
+  return reduce(
+    arr,
+    function (max, el) {
+      if (foo(max) > foo(el)) return max;
+      return el;
+    },
+    init
+  );
 }
 
 function max(arr, init) {
@@ -30,14 +33,19 @@ function maxJS(arr, init) {
   return maxKeyJS(arr, init, (x) => x);
 }
 
-console.log('Lodash max', max(arr, 0));
-console.log('JS max', maxJS(arr, 0));
+console.log("Lodash max", max(arr, 0));
+console.log("JS max", maxJS(arr, 0));
 
 // 객체 결과
 const MIN = { total: 0 };
-console.log('Lodash maxKey', maxKey(objArr, MIN, (el) => el.total));
-console.log('JS maxKey', maxKeyJS(objArr, MIN, (el) => el.total));
-
+console.log(
+  "Lodash maxKey",
+  maxKey(objArr, MIN, (el) => el.total)
+);
+console.log(
+  "JS maxKey",
+  maxKeyJS(objArr, MIN, (el) => el.total)
+);
 
 // 연습문제 2
 function bigSpenders(customers) {
@@ -58,3 +66,46 @@ function isPurchaseMoreTwice(customer) {
 function average(numbers) {
   return numbers.reduce((acc, cur) => acc + cur, 0) / numbers.length;
 }
+
+function concat(arrs) {
+  const ret = [];
+  forEach(arrs, function (arr) {
+    forEach(arr, function (el) {
+      ret.push(el);
+    });
+  });
+  return ret;
+}
+
+console.log(
+  concat([
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+  ])
+);
+
+const testObj = {};
+
+const position = "pitcher";
+
+testObj[position] = "Tetz";
+
+console.log(testObj);
+
+const em = ["jane", "tom"];
+
+function recommendPosition(name) {
+  if (name === "jane") return "pitcher";
+  return "catcher";
+}
+
+const recommendations = em.reduce((acc, cur, idx) => {
+  acc.push({ name: cur, position: recommendPosition(cur) });
+  return acc;
+}, []);
+
+console.log(recommendations);
+
+const newObj = { ...testObj, scroe: 0 };
+console.log(newObj);
