ee6a635e99f88b0d36352edeb1b2ebfe1266376a
xenosign <xenosign@naver.com>
2023-12-10 03:00:26 +0900

    Update test.java

--- 5w/test.java
+++ 5w/test.java
@@ -15,8 +15,38 @@ public Article updateArticle(Article article) {
   return repository.save(existing);
 }
 
-// 게시글 수정하는 api를 java spring 으로 만들어줘.
+// 게시글 수정하는 api를 java spring 으로 만들어줘..
 
+// GPT 3.5
+// 필요한 패키지 import
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    // 게시글 수정을 위한 API 엔드포인트
+    @PutMapping("/{postId}")
+    public ResponseEntity<String> updatePost(@PathVariable Long postId, @RequestBody PostRequestDto requestDto) {
+        // postId에 해당하는 게시글을 데이터베이스에서 조회
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("게시글을 찾을 수 없습니다."));
+
+        // requestDto에서 수정할 내용을 가져와서 게시글 업데이트
+        post.setTitle(requestDto.getTitle());
+        post.setContent(requestDto.getContent());
+
+        // 수정된 게시글을 데이터베이스에 저장
+        postRepository.save(post);
+
+        // 수정 성공 응답 반환
+        return ResponseEntity.status(HttpStatus.OK).body("게시글이 성공적으로 수정되었습니다.");
+    }
+}
+
+// GPT 4.0
 // 필요한 패키지 import
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
